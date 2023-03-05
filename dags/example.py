280c13de9c2e421ab0f6b8011aeaf000f28c90c1
tkosht <takehito.oshita.business@gmail.com>
2023-03-05 19:52:16 +0900

    mod: example

--- dags/example.py
+++ dags/example.py
@@ -8,7 +8,7 @@ from airflow.operators.bash import BashOperator
 with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
 
     # Tasks are represented as operators
-    hello = BashOperator(task_id="hello", bash_command="echo hello")
+    hello = BashOperator(task_id="hello_task", bash_command="echo hello")
 
     @task()
     def airflow():
